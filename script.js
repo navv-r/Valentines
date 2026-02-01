5bad783b9be215f8244eac97cada14457a25a72e
navv-r <navendra604@gmail.com>
2026-01-31 22:21:19 -0500

    getting there

--- script.js
+++ script.js
@@ -2,36 +2,32 @@
 document.querySelector('.yes').addEventListener('click', function() {
     const image = document.querySelector('.yes__img');
     
-    
     image.style.display = 'block'; 
     setTimeout(() => {
         image.style.opacity = 1; 
     }, 10); 
 
-    
     setTimeout(() => {
         image.style.opacity = 0; 
         setTimeout(() => {
             image.style.display = 'none'; 
         }, 500); 
-    }, 7000); 
+    }, 4000); 
 });
 
 // NO BUTTON
 document.querySelector('.no').addEventListener('click', function() {
     const image = document.querySelector('.no__img');
     
-    
     image.style.display = 'block'; 
     setTimeout(() => {
         image.style.opacity = 1; 
     }, 10); 
 
-   
     setTimeout(() => {
         image.style.opacity = 0; 
         setTimeout(() => {
             image.style.display = 'none'; 
         }, 500); 
-    }, 7000);
+    }, 4000);
 });
